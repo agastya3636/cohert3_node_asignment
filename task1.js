2702019ecaa0afc957c463630db9333d004ad64b
Agastya Kumar Yadav <124435030+agastya3636@users.noreply.github.com>
2024-08-24 20:55:36 +0530

    task 1 done

--- task1.js
+++ task1.js
@@ -3,24 +3,22 @@ const { Command } = require('commander');
 const program = new Command();
 
 program
-  .name('counter')
-  .description('CLI to do file based tasks')
-  .version('0.8.0');
+  .name('word-counter')
+  .description('CLI to count the number of words in a file')
+  .version('1.0.0');
 
-program.command('count')
-  .description('Count the number of lines in a file')
-  .argument('<file>', 'file to count')
+program
+  .argument('<file>', 'file path to count words in')
   .action((file) => {
     fs.readFile(file, 'utf8', (err, data) => {
       if (err) {
-        console.log(err);
+        console.error(`Error reading file: ${err.message}`);
+        process.exit(1);  // Exit with an error code
       } else {
-        const lines = data.split('\n').length;
-        console.log(`There are ${lines} lines in ${file}`);
+        const words = data.split(/\s+/).filter(Boolean);
+        console.log(`You have ${words.length} words in this file.`);
       }
     });
   });
-
-  
 
 program.parse();
